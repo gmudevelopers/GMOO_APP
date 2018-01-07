7ec24f54673bcd7d1fbe2ed489990c4e7110f59d
Eric Opoku <eric.opoku20@yahoo.com>
2018-01-07 08:47:00 -0500

    Added serial version uid to domains as well as equals hashcode and tostring

--- app/src/main/java/gmoo/com/gmudevelopers/edu/gmoo/model/ItemObject.java
+++ app/src/main/java/gmoo/com/gmudevelopers/edu/gmoo/model/ItemObject.java
@@ -1,16 +1,28 @@
 package gmoo.com.gmudevelopers.edu.gmoo.model;
 
+import java.io.Serializable;
+
 /**
  * Created by daniel on 1/5/18.
  */
 
-public class ItemObject {
+public class ItemObject implements Serializable {
+
+    /**
+     * Serial Version unique id needed for serialization across multiple JVM
+     */
+    private static final long serialVersionUID = -6618815665851099001L;
 
     private String name;
+
     private int photo;
 
     public ItemObject(int photo) {
+        this.photo = photo;
+    }
 
+    public ItemObject(String name, int photo) {
+        this.name = name;
         this.photo = photo;
     }
 
@@ -28,5 +40,32 @@ public class ItemObject {
 
     public void setPhoto(int photo) {
         this.photo = photo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ItemObject that = (ItemObject) o;
+
+        if (photo != that.photo) return false;
+        return name != null ? name.equals(that.name) : that.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + photo;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ItemObject{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", photo=").append(photo);
+        sb.append('}');
+        return sb.toString();
     }
 }
