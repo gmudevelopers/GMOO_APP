8e53a4924d8c7cb11c160aee92bf18a92b5f8e6b
Daniel Boakye <countrybus1@gmail.com>
2018-03-04 17:41:23 -0500

    allow user to select multiple photos

---
+++ app/src/main/java/gmoo/com/gmudevelopers/edu/gmoo/adapters/PostAddPhotoListAdapter.java
@@ -0,0 +1,8 @@
+package gmoo.com.gmudevelopers.edu.gmoo.adapters;
+
+/**
+ * Created by daniel on 1/11/18.
+ */
+
+public class PostAddPhotoAdapter {
+}
---
+++ app/src/main/java/gmoo/com/gmudevelopers/edu/gmoo/model/Photo.java
@@ -0,0 +1,8 @@
+package gmoo.com.gmudevelopers.edu.gmoo.model;
+
+/**
+ * Created by daniel on 1/11/18.
+ */
+
+public class Photo {
+}
